0785031cb01d6e0df8359588f473315f2f1fce8d
4mh23cs014-cpu <4mh23cs014@gmail.com>
2026-01-29 13:01:34 +0530

    Preinstall numpy in build.sh to avoid scikit-learn source builds

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Upgrade packaging tools first
 pip install --upgrade pip setuptools wheel
+
+# Install a compatible numpy wheel first to satisfy scikit-learn build deps
+pip install --upgrade "numpy==1.24.3"
+
+# Then install remaining requirements
 pip install -r requirements.txt
